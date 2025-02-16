78edf655cc369aeb34cac459f12245d78a66b874
haris shafi <146629838+harisshafi10@users.noreply.github.com>
2025-02-16 19:16:48 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -6,6 +6,19 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+const allowedOrigins = [
+  'https://ai-powered-code-reviewer-1.onrender.com', // Frontend URL
+];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res)=>{
     res.send('hellow world')
 })
